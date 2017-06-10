198b990e82532701484dcfc5eb7d05e02115f0f7
WhyNOTnow <mishkakovrishka2222@gmail.com>
2017-06-10 11:32:33 +0300

    Create Myform.h

---
+++ Myform.h
@@ -0,0 +1,6 @@
+#include <Windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+int showSV(DWORD pid, char* name);
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+	
